7c2da601f846cbdb6afe9b351032ce932cb82a88
Long <longlong2000hcm@gmail.com>
2020-03-02 06:27:54 +0200

    add remote database

--- db/knexConfiguration.js
+++ db/knexConfiguration.js
@@ -1,10 +1,15 @@
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-      host : 'localhost',
+      // host : 'localhost',
+      // user : 'root',
+      // password : 'root',
+      // database : 'exam_platform'
+      host : '35.228.126.82',
       user : 'root',
       password : 'root',
-      database : 'exam_platform'
+      database : 'exam_platform',
+      // port: 3306
     }
   });
 
